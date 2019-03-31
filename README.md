5c255c8e61d35e1d8e4645eec7205218a4edea39
Iman Akbari <imakbari@gmail.com>
2019-04-01 01:34:20 +0200

    Genesis

---
+++ README.md
@@ -0,0 +1,8 @@
+
+FilePath
+========
+
+FilePath is an object-oriented wrapper for file and directory paths in 
+python which is used to avoid the os.path.something mess we've all done
+at some point.
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='file-path',
+    packages=['filepath'],
+    version='0.1a',
+    description='Object-oriented wrapper for paths',
+    author='Iman Akbari',
+    author_email='imakbari@gmail.com',
+    url='https://github.com/blackvvine/file-path',
+    download_url='https://github.com/blackvvine/file-path/tarball/1.0',
+    keywords=['file', 'directory'],
+    classifiers=[],
+)
